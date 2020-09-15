35e88f251b7464a28b9554603bff2afe8c6d2be1
Plimon Howard V <plimon.howard@gmail.com>
2020-09-15 21:13:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
 
+array = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
